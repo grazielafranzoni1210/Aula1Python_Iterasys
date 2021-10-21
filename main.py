39ab01f2a2f28f0672db47b18ac3eae8a236c32c
grazielafranzoni1210 <51487754+grazielafranzoni1210@users.noreply.github.com>
2021-10-20 22:16:07 -0300

    Update main.py
    "Correção da aula 1"

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-# imports / bibliotecas
+# 1 - imports / bibliotecas
 
-# classes
+# 2 - Classe
 
-# métodos e funções
-
+# 3 -  Métodos e Funções
 def print_hi(name):
-    print(f'Oi, {name}')
+    print(f'Oi, {name}')  # a partir do Python 3
+
 
 def calcular_area_do_retangulo(largura, comprimento):
     return largura * comprimento
 
+
 def calcular_area_do_quadrado(lado):
     return lado ** 2
+
 
 def calcular_area_do_triangulo(largura, comprimento):
     return largura * comprimento / 2
 
+
 def contagem_progressiva(fim):
-    for numero in range(fim): # repetir o bloco de zero até o fim
-        print(numero)         # exibir o número
+    for numero in range(fim):  # repetir o bloco de zero até o fim
+        print(numero)  # exibir o número
+
 
 def apoiar_candidato(nome, vezes):
     for numero in range(vezes):
+        # contador = numero + 1
+        # print(f'{contador} - {nome}')
 
-
-        if numero < 10:
-            print(f'00{numero}', '-', nome)
+        if numero < 9:
+            print(f'00{numero + 1} - {nome}')
         elif numero < 99:
-            print(f'0{numero}', '-', nome)
+            print(f'0{numero + 1} - {nome}')
         else:
             print(numero + 1, '-', nome)
 
+
 def brincar_de_plim(fim):
     for numero in range(fim):
-        if numero % 4  == 1:
+        if numero % 4 == 0:
             print('PLIM!')
         else:
-            print('{:0>3}'.format(numero+1))
+            print('{:0>3}'.format(numero))
+
+
+def brincar_de_para_ou_continua():
+    resposta = 'C'  # S aqui significa que continua
+
+    # while resposta == 'C' or resposta == 'c':
+    while resposta.upper() == 'C':
+        resposta = input("Digite C para continuar ou qualquer outro caracter para parar")
+
+    print('Você decidiu parar com a brincadeira')
 
 
 # estrutura de identificação/execução
@@ -52,7 +68,7 @@ resultado = calcular_area_do_quadrado(5)
 print(f'A área do quadrado é de {resultado} m²')
 
 # chamar a função de cálculo da área do triângulo
-resultado = calcular_area_do_triangulo(6,7)
+resultado = calcular_area_do_triangulo(6, 7)
 print(f'A área do triângulo é de {resultado} m²')
 
 # executar uma contagem progressiva
@@ -64,3 +80,5 @@ apoiar_candidato('Faker', 101)
 # brincar de plim
 brincar_de_plim(101)
 
+# exemplo com while - para ou continua
+brincar_de_para_ou_continua()
